69f1b3e434786ac2427542c0d711fba3e8048015
atheimuz <atheimuz@naver.com>
2024-11-17 23:43:45 +0900

    feat: 카테고리 수정

--- src/db/category.ts
+++ src/db/category.ts
@@ -1,8 +1,10 @@
 const category: { label: string; value: string; image: string }[] = [
-    { label: "디자인 컴포넌트", value: "ui", image: "book.png" },
+    { label: "스타일", value: "style", image: "brush.png" },
+    // { label: "디자인 컴포넌트", value: "ui", image: "book.png" },
     { label: "데이터 요청", value: "remote", image: "rocket.png" },
     { label: "커스텀훅", value: "hook", image: "gift-box.png" },
-    { label: "에러", value: "error", image: "error.png" }
+    { label: "에러", value: "error", image: "error.png" },
+    { label: "유틸 함수", value: "util", image: "puzzle.png" }
 ];
 
 export default category;
