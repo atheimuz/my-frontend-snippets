c01f06a86731af27236241d8c05dd9a805515145
atheimuz <atheimuz@naver.com>
2024-12-19 00:44:23 +0900

    feat: 코드 파일 리스트 비동기로 가져오기

--- src/app/(main)/@code/page.tsx
+++ src/app/(main)/@code/page.tsx
@@ -1,12 +1,35 @@
-import code from "@/db/code";
+"use client";
+
+import { useState, useEffect } from "react";
+import { ICodeFile } from "@/models/code";
 import CodeRenderer from "@/components/CodeRenderer";
 import styles from "./page.module.scss";
+import { useSearchParams } from "next/navigation";
 
-export default function CodePage({ searchParams }: { searchParams: { codeName?: string } }) {
-    const codeName = searchParams?.codeName;
-    if (!codeName || !code[codeName]) return null;
+export default function CodePage() {
+    const searchParams = useSearchParams();
+    const codeName = searchParams.get("codeName");
+    const [dataList, setDataList] = useState<ICodeFile[] | null>(null);
 
-    const dataList = code[codeName];
+    const loadData = async () => {
+        if (!codeName) {
+            return setDataList(null);
+        }
+        const data = (await import("@/db/code")).default;
+        const newDataList = data[codeName];
+
+        if (newDataList) {
+            setDataList(newDataList);
+        } else {
+            setDataList(null);
+        }
+    };
+
+    useEffect(() => {
+        loadData();
+    }, [codeName]);
+
+    if (!dataList) return null;
 
     return (
         <div className={styles.page}>
